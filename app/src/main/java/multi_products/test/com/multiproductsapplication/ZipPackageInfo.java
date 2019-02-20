f6f8378c2981ae651ae324c7c084ab385e68adeb
jun <>
2019-02-20 11:25:07 +0800

    代码修改

---
+++ app/src/main/java/multi_products/test/com/multiproductsapplication/ZipPackageInfo.java
@@ -0,0 +1,11 @@
+package multi_products.test.com.multiproductsapplication;
+
+/**
+ * multi_products.test.com.multiproductsapplication
+ *
+ * @author jun
+ * @date 2019/2/20
+ * Copyright (c) 2019 ${ORGANIZATION_NAME}. All rights reserved.
+ */
+public class ZipPackageInfo {
+}
